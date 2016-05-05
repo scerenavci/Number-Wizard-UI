f976b2ebd4188ab6762f2ce56276244833b6e08d
Ceren Avci <s.cerenavci@gmail.com>
2016-05-05 11:51:06 +0300

    Added NumberWizard.cs

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max ;
+	int min ;
+	int guess ;	
+	public int maxGuessesAllowed = 5;
+
+	public Text text;
+	
+	void Start () {
+	
+		StartGame();
+		
+	}
+	
+	void StartGame () {
+	
+		max = 1000;
+		min = 1;
+		guess =500;		
+		max = max + 1;		
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+	public void GuessHigher(){	
+		min = guess;
+		NextGuess();
+	}
+	
+	void NextGuess () {	
+		guess = (max + min )/2;
+		text.text = guess.ToString();
+		maxGuessesAllowed --;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
