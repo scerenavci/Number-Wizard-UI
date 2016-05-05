645708f0586bf795501e3c8f267f9a108bce4793
Ceren Avci <s.cerenavci@gmail.com>
2016-05-05 11:02:07 +0300

    Updated all scenes

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -7,10 +7,12 @@ public class LevelManager : MonoBehaviour {
 	public void LoadLEvel (string name){
 	
 		Debug.Log("Level load requested for: " +name);
+		Application.LoadLevel(name);
 	}
 	
 	public void QuitRequest(){
 	
 		Debug.Log("I wanna quit !");
+		Application.Quit();
 	}
 }
